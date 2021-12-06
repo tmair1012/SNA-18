48da3b62afbe7f8f2f25d4afea7d10d146b0af17
TylerMair <tyleramair@gmail.com>
2021-12-06 09:27:32 -0700

    add and remove reaction in thought controller, need to populate controllers

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -52,6 +52,31 @@ const thoughtController = {
         Thought.findOneAndDelete({ _id: params.id })
         .then(dbThoughtData => res.json(dbThoughtData))
         .catch(err => res.json(err));
+    },
+
+    addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body }},
+            { new: true, runValidators: true}
+        )
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({ message: 'No Thought with this ID exists '});
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.json(err));
+    },
+
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reaction: { reactionId: params.reactionId } } },
+            { new: true }
+            )
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => res.json(err));
     }
 };
 
